3bee89c6f1db2d7fad4e87ed14dd437a418e6a0a
Pedro Martinez R <104815353+vnz5k@users.noreply.github.com>
2023-04-20 20:48:37 -0600

    Update GrafoMatrizAdyacencia.java

--- GrafoMatrizAdyacencia.java
+++ GrafoMatrizAdyacencia.java
@@ -1,52 +1,40 @@
 import java.util.*;
 
 public class GrafoMatrizAdyacencia {
-    private int[][] matriz;
+    private boolean[][] matrizAdyacencia;
     private boolean dirigido;
 
-    // Constructor
-    public GrafoMatrizAdyacencia(int vertices, boolean dirigido) {
-        matriz = new int[vertices][vertices];
+    public GrafoMatrizAdyacencia(int numVertices, boolean dirigido) {
+        matrizAdyacencia = new boolean[numVertices][numVertices];
         this.dirigido = dirigido;
     }
 
-    // Agrega arista
     public void agregarArista(int origen, int destino) {
-        matriz[origen][destino] = 1;
+        matrizAdyacencia[origen][destino] = true;
         if (!dirigido) {
-            matriz[destino][origen] = 1;
+            matrizAdyacencia[destino][origen] = true;
         }
     }
 
-    // Determina si el grafo es conexo
     public boolean esConexo() {
-        boolean[] visitados = new boolean[matriz.length];
+        Set<Integer> visitados = new HashSet<>();
         dfs(0, visitados);
-
-        for (boolean visitado : visitados) {
-            if (!visitado) {
-                return false;
-            }
-        }
-        return true;
+        return visitados.size() == matrizAdyacencia.length;
     }
 
-    // Función DFS para recorrer el grafo
-    private void dfs(int vertice, boolean[] visitados) {
-        visitados[vertice] = true;
-
-        for (int i = 0; i < matriz.length; i++) {
-            if (matriz[vertice][i] == 1 && !visitados[i]) {
+    private void dfs(int vertice, Set<Integer> visitados) {
+        visitados.add(vertice);
+        for (int i = 0; i < matrizAdyacencia.length; i++) {
+            if (matrizAdyacencia[vertice][i] && !visitados.contains(i)) {
                 dfs(i, visitados);
             }
         }
     }
 
-    // Determina si el grafo es completo
     public boolean esCompleto() {
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz.length; j++) {
-                if (i != j && matriz[i][j] == 0) {
+        for (int i = 0; i < matrizAdyacencia.length; i++) {
+            for (int j = 0; j < matrizAdyacencia.length; j++) {
+                if (i != j && !matrizAdyacencia[i][j]) {
                     return false;
                 }
             }
@@ -54,62 +42,62 @@ public class GrafoMatrizAdyacencia {
         return true;
     }
 
-    // Obtiene el grado de cada vértice
     public List<Integer> gradoVertices() {
-        List<Integer> grados = new ArrayList<>();
-
-        for (int i = 0; i < matriz.length; i++) {
-            int grado = 0;
-            for (int j = 0; j < matriz.length; j++) {
-                if (matriz[i][j] == 1) {
-                    grado++;
+        List<Integer> grado = new ArrayList<>();
+        for (int i = 0; i < matrizAdyacencia.length; i++) {
+            int gradoActual = 0;
+            for (int j = 0; j < matrizAdyacencia.length; j++) {
+                if (matrizAdyacencia[i][j]) {
+                    gradoActual++;
                 }
             }
-            grados.add(grado);
+            grado.add(gradoActual);
         }
-        return grados;
+        return grado;
     }
 
-    // Encuentra un camino entre dos vértices
     public List<Integer> camino(int origen, int destino) {
         List<Integer> camino = new ArrayList<>();
-        boolean[] visitados = new boolean[matriz.length];
-        dfsCamino(origen, destino, visitados, camino);
+        dfsCamino(origen, destino, new HashSet<>(), camino);
         return camino;
     }
 
-    // Función DFS para encontrar un camino
-    private boolean dfsCamino(int vertice, int destino, boolean[] visitados, List<Integer> camino) {
-        visitados[vertice] = true;
-        camino.add(vertice);
+    private boolean dfsCamino(int origen, int destino, Set<Integer> visitados, List<Integer> camino) {
+        visitados.add(origen);
+        camino.add(origen);
 
-        if (vertice == destino) {
+        if (origen == destino) {
             return true;
         }
 
-        for (int i = 0; i < matriz.length; i++) {
-            if (matriz[vertice][i] == 1 && !visitados[i]) {
+        for (int i = 0; i < matrizAdyacencia.length; i++) {
+            if (matrizAdyacencia[origen][i] && !visitados.contains(i)) {
                 if (dfsCamino(i, destino, visitados, camino)) {
                     return true;
                 }
             }
         }
+
         camino.remove(camino.size() - 1);
         return false;
     }
 
-    // Encuentra el número de componentes conexos
     public int numeroComponentesConexos() {
         int componentesConexos = 0;
-        boolean[] visitados = new boolean[matriz.length];
+        Set<Integer> visitados = new HashSet<>();
 
-        for (int i = 0; i < matriz.length; i++) {
-            if (!visitados[i]) {
+        for (int i = 0; i < matrizAdyacencia.length; i++) {
+            if (!visitados.contains(i)) {
                 dfs(i, visitados);
                 componentesConexos++;
             }
         }
+
         return componentesConexos;
+    }
+
+    public int getNumeroVertices() {
+        return matrizAdyacencia.length;
     }
 
     public static void main(String[] args) {
@@ -126,12 +114,11 @@ public class GrafoMatrizAdyacencia {
 
             if (opcion == 1) {
                 System.out.print("Ingrese el número de vértices: ");
-                int vertices = scanner.nextInt();
+                int numVertices = scanner.nextInt();
                 System.out.print("¿Es dirigido? (true/false): ");
                 boolean dirigido = scanner.nextBoolean();
-                grafo = new GrafoMatrizAdyacencia(vertices, dirigido);
+                grafo = new GrafoMatrizAdyacencia(numVertices, dirigido);
 
-                int subOpcion;
                 while (true) {
                     System.out.println("\nMenú de operaciones:");
                     System.out.println("1. Agregar arista");
@@ -142,38 +129,38 @@ public class GrafoMatrizAdyacencia {
                     System.out.println("6. Mostrar número de componentes conexos");
                     System.out.println("7. Regresar al menú principal");
                     System.out.print("Ingrese la opción deseada: ");
-                    subOpcion = scanner.nextInt();
+                    opcion = scanner.nextInt();
 
-                    if (subOpcion == 1) {
+                    if (opcion == 1) {
                         System.out.print("Ingrese el vértice origen: ");
                         int origen = scanner.nextInt();
                         System.out.print("Ingrese el vértice destino: ");
                         int destino = scanner.nextInt();
                         grafo.agregarArista(origen, destino);
-                    } else if (subOpcion == 2) {
+                    } else if (opcion == 2) {
                         System.out.println("¿Es conexo? " + grafo.esConexo());
-                    } else if (subOpcion == 3) {
+                    } else if (opcion == 3) {
                         System.out.println("¿Es completo? " + grafo.esCompleto());
-                    } else if (subOpcion == 4) {
+                    } else if (opcion == 4) {
                         System.out.println("Grado de vértices: " + grafo.gradoVertices());
-                    } else if (subOpcion == 5) {
+                    } else if (opcion == 5) {
                         System.out.print("Ingrese el vértice origen: ");
                         int origen = scanner.nextInt();
                         System.out.print("Ingrese el vértice destino: ");
                         int destino = scanner.nextInt();
-                        System.out.println("Camino entre " + origen + " y " + destino + ": " + grafo.camino(origen, destino));
-                    } else if (subOpcion == 6) {
+                        if (origen >= 0 && origen < grafo.getNumeroVertices() && destino >= 0 && destino < grafo.getNumeroVertices()) {
+                            System.out.println("Camino entre " + origen + " y " + destino + ": " + grafo.camino(origen, destino));
+                        } else {
+                            System.out.println("Los vértices ingresados están fuera de rango.");
+                        }
+                    } else if (opcion == 6) {
                         System.out.println("Número de componentes conexos: " + grafo.numeroComponentesConexos());
-                    } else if (subOpcion == 7) {
+                    } else if (opcion == 7) {
                         break;
-                    } else {
-                        System.out.println("Opción inválida. Intente de nuevo.");
                     }
                 }
             } else if (opcion == 2) {
                 break;
-            } else {
-                System.out.println("Opción inválida. Intente de nuevo.");
             }
         }
     }
